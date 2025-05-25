9c414df70b757dfe179f6738d5460b1989b05074
rikkiter <rika5637@mail.ru>
2025-05-25 19:48:11 +0300

    binaryTree

--- binaryTree.py
+++ binaryTree.py
@@ -42,21 +42,71 @@ class BinaryTree:
         else:
             return self.search(value, branch.right, False)
 
-    def remove(self, value: int, branch=None):
+    def remove(self, value: int, branch=None, flag=True):
         if branch is None:
-            branch = self.head
-        if value == branch.value:
-            left = branch.left
-            prev.right = branch.right
+            if flag:
+                branch = self.head
+            else:
+                return f"No {value} in Tree"
+        if branch.right is not None and value == branch.right.value:
+            if branch.right.right is not None:
+                left = branch.right.left
+                branch.right.value = branch.right.right.value
+                branch.right = branch.right.right
+                temp = branch.right
+                while True:
+                    if temp.left is None:
+                        temp.left = left
+                        return
+                    else:
+                        temp = temp.left
+            elif branch.right.left is not None:
+                branch.right.value = branch.right.left.value
+                branch.right = branch.right.left
+                return
+            else:
+                branch.right = None
+                return
+        if branch.left is not None and value == branch.left.value:
+            if branch.left.right is not None:
+                left = branch.left.left
+                branch.left.value = branch.left.right.value
+                branch.left = branch.left.right
+                temp = branch.left
+                while True:
+                    if temp.left is None:
+                        temp.left = left
+                        return
+                    else:
+                        temp = temp.left
+            elif branch.left.left is not None:
+                branch.left.value = branch.left.left.value
+                branch.left = branch.left.left
+                return
+            else:
+                branch.left = None
+                return
         if value < branch.value:
-            return self.search(value, branch.left)
-        else:
-            return self.search(value, branch.right)
+            return self.remove(value, branch.left, False)
+        if value > branch.value:
+            return self.remove(value, branch.right, False)
 
 bt = BinaryTree()
+bt.add(17)
 bt.add(5)
+bt.add(25)
+bt.add(19)
+bt.add(30)
+bt.add(28)
+bt.add(70)
 bt.add(3)
-bt.add(7)
 bt.add(9)
 print(bt.head.right.value)
 print(bt.search(0))
+print(bt.search(19))
+print(bt.search(70))
+print(bt.remove(30))
+print(bt.search(30))
+print(bt.search(70))
+print(bt.remove(28))
+print(bt.search(19))
